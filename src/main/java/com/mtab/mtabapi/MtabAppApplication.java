2e1a37d7663c69423bb594a77d67fa1842987371
jarzeckil <jarzeckil@gmail.com>
2025-12-08 18:55:15 +0100

    deleted imports

--- src/main/java/com/mtab/mtabapi/MtabAppApplication.java
+++ src/main/java/com/mtab/mtabapi/MtabAppApplication.java
@@ -1,15 +1,7 @@
 package com.mtab.mtabapi;
 
-import com.mtab.mtabapi.entity.Customer;
-import com.mtab.mtabapi.entity.Item;
-import com.mtab.mtabapi.entity.ItemCategory;
-import com.mtab.mtabapi.repository.CustomerRepository;
-import com.mtab.mtabapi.repository.ItemCategoryRepository;
-import com.mtab.mtabapi.repository.ItemRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MtabAppApplication {
